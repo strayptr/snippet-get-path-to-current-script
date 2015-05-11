a096ca5c66061a31ce42560df62f16937dc28952
strayptr <strayptr@gmail.com>
2015-05-11 17:15:34 -0500

    etc/common.sh utilities.

--- etc/example.sh
+++ etc/example.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-#==============================================================================
-# Get the full path to this scriptfile, and also load our project's
-# global constants.
-#==============================================================================
-scriptpath="`realpath \"$0\"`"
-scriptname="`basename \"$scriptpath\"`"
-scriptpath="`dirname \"$scriptpath\"`"
+# include common functionality.
+source "`dirname "$BASH_SOURCE"`"/common.sh
 
-source "$scriptpath/config.sh"
-
-#------------------------------------------------------------------------------
+#==============================================================================
 # Fail if any of our required vars are missing.
 # See http://unix.stackexchange.com/questions/121379/test-if-multiple-variables-are-set
-#------------------------------------------------------------------------------
+#==============================================================================
+depends_on                        \
+  ProjectName ProjectVersion
+
 
 # This script requires the following vars:
-for var in                      \
-  ProjectName                   \
-  ProjectVersion                \
-  ; do
-  # see http://unix.stackexchange.com/questions/41292/variable-substitution-with-an-exclamation-mark-in-bash/41293#41293
-  if [ -z "${!var}" ] ; then
-      echo "\${$var} is required but isn't set.  Check that etc/config.sh is set up correctly."
-      exit 1
-  fi
-done
+# for var in                      \
+#   ScriptPath ScriptName         \
+#   ProjectName ProjectVersion    \
+#   ; do
+#   # see http://unix.stackexchange.com/questions/41292/variable-substitution-with-an-exclamation-mark-in-bash/41293#41293
+#   if [ -z "${!var}" ] ; then
+#       echoerr "\${$var} is required but isn't set.  Check that etc/config.sh is set up correctly."
+#       exit 1
+#   fi
+# done
 
 #==============================================================================
 # Perform some script-specific tasks.
